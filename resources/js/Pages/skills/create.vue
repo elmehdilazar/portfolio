7b4ae3a9e7a2676729fad83cc31a33ea3ddf0c4d
elmehdilazar <96011628+elmehdilazar@users.noreply.github.com>
2024-02-06 21:26:31 +0100

    backend add skill create and index  vue

---
+++ resources/js/Pages/skills/create.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+
+        <Head title="Dashboard" />
+        <AuthenticatedLayout>
+            <template #header>
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">New skill</h2>
+            </template>
+
+            <div class="py-12">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="flex justify-end m-2 p-2">
+                       Form
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    </div>
+</template>
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Link, Head } from '@inertiajs/vue3';
+
+</script>
