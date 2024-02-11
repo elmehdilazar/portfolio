13524ff033432edb1861bb49ac8d369fe2889326
elmehdilazar <96011628+elmehdilazar@users.noreply.github.com>
2024-02-11 01:31:06 +0100

    frontend create add forms and post data

--- resources/js/Pages/skills/create.vue
+++ resources/js/Pages/skills/create.vue
@@ -1,24 +1,73 @@
 <template>
     <div>
 
-        <Head title="Dashboard" />
+        <Head title="new skill" />
         <AuthenticatedLayout>
             <template #header>
-                <h2 class="font-semibold text-xl text-gray-800 leading-tight">New skill</h2>
+                <h2 class="text-xl font-semibold leading-tight text-gray-800">New skill</h2>
             </template>
+<div class="py-12">
+    <div class="max-w-md mx-auto bg-white sm:px-6 lg:px-8">
 
-            <div class="py-12">
-                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div class="flex justify-end m-2 p-2">
-                       Form
-                    </div>
-                </div>
+            <form class="p-4" @submit.prevent="submit">
+            <div class="">
+                <InputLabel for="name" value="Name" />
+
+                <TextInput
+                    id="name"
+                    type="text"
+                    class="block w-full mt-1"
+                    v-model="form.name"
+                    required
+                    autofocus
+                    autocomplete="username"
+                />
+
+                <InputError class="mt-2" :message="form.errors.name" />
             </div>
+            <div class="mt-2">
+                <InputLabel for="image" value="Image" />
+
+                <TextInput
+                    id="image"
+                    type="file"
+                    @input="form.image = $event.target.files[0]"
+                />
+
+                <InputError class="mt-2" :message="form.errors.image" />
+            </div>
+
+
+
+
+
+            <div class="flex items-center justify-end mt-4">
+
+
+                <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                   Store
+                </PrimaryButton>
+            </div>
+        </form>
+    </div>
+</div>
         </AuthenticatedLayout>
     </div>
 </template>
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Link, Head } from '@inertiajs/vue3';
+import Checkbox from '@/Components/Checkbox.vue';
 
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Link, Head, useForm } from '@inertiajs/vue3';
+const form = useForm({
+    name: '',
+    image:null,
+});
+const submit = () => {
+    form.post(route('skills.store'));
+};
 </script>
